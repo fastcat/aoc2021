0fe661c2ba9e3bab715726bbadcd0e3bdc1a637a
Matthew Gabeler-Lee <cheetah@fastcat.org>
2021-12-14 20:37:45 -0500

    day 5, part 2

--- day5/evaluate.go
+++ day5/evaluate.go
@@ -1,14 +1,19 @@
 package day5
 
-func GridOverlaps(lines []Line) map[Point]int {
+import (
+	"fmt"
+	"math"
+)
+
+func GridOverlaps(lines []Line, includeDiagonal bool) map[Point]int {
 	grid := make(map[Point]int)
 	for _, l := range lines {
-		updateGrid(grid, l)
+		updateGrid(grid, l, includeDiagonal)
 	}
 	return grid
 }
 
-func updateGrid(grid map[Point]int, line Line) {
+func updateGrid(grid map[Point]int, line Line, includeDiagnoal bool) {
 	if line.Start.X == line.End.X {
 		start, end := line.Start.Y, line.End.Y
 		if end < start {
@@ -25,9 +30,23 @@ func updateGrid(grid map[Point]int, line Line) {
 		for p := (Point{start, line.Start.Y}); p.X <= end; p.X++ {
 			grid[p] = grid[p] + 1
 		}
+	} else if math.Abs(float64(line.End.X-line.Start.X)) == math.Abs(float64(line.End.Y-line.Start.Y)) {
+		if includeDiagnoal {
+			start, end := line.Start, line.End
+			if start.X > end.X {
+				start, end = end, start
+			}
+			// x is now increasing, but y may be decreasing
+			yDelta := 1
+			if start.Y > end.Y {
+				yDelta = -1
+			}
+			for p := start; p.X <= end.X; p.X, p.Y = p.X+1, p.Y+yDelta {
+				grid[p] = grid[p] + 1
+			}
+		}
 	} else {
-		// panic(fmt.Errorf("diagonal line: %v", line))
-		// continue
+		panic(fmt.Errorf("wonky diagonal line: %v", line))
 	}
 }
 
--- day5/part2_test.go
+++ day5/part2_test.go
@@ -7,19 +7,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestExample1(t *testing.T) {
+func TestExample2(t *testing.T) {
 	lines, err := ParseLines(exampleInput)
 	require.NoError(t, err)
-	grid := GridOverlaps(lines, false)
+	grid := GridOverlaps(lines, true)
 	const threshold = 2
 	nAbove := NumPointsAtLeast(grid, threshold, t.Logf)
-	assert.Equal(t, 5, nAbove)
+	assert.Equal(t, 12, nAbove)
 }
 
-func TestChallenge1(t *testing.T) {
+func TestChallenge2(t *testing.T) {
 	lines, err := ParseLines(challengeInput)
 	require.NoError(t, err)
-	grid := GridOverlaps(lines, false)
+	grid := GridOverlaps(lines, true)
 	const threshold = 2
 	nAbove := NumPointsAtLeast(grid, threshold, nil)
 	t.Logf("found %d points >= %d", nAbove, threshold)
