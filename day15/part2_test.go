ef00fe19d93abddaffb7c33c11e87272760bab36
Matthew Gabeler-Lee <cheetah@fastcat.org>
2022-01-06 01:58:40 -0500

    day 15, part 2 (fast, but buggy)

--- day15/part2_test.go
+++ day15/part2_test.go
@@ -1,11 +1,28 @@
 package day15
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/fastcat/aoc2021/util"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func costMapStr(m *util.IntBoard) string {
+	b := strings.Builder{}
+	for i := m.Iterator(); i.Valid(); i = i.NextRC() {
+		pt := i.Point()
+		if pt.C == 0 && pt.R > 0 {
+			b.WriteRune('\n')
+		} else if pt.C > 0 {
+			b.WriteRune(' ')
+		}
+		fmt.Fprintf(&b, "%3d", i.Value())
+	}
+	return b.String()
+}
 
 func TestPart2Example(t *testing.T) {
 	b, err := util.ParseIntBoardCompact(exampleInput)
@@ -13,6 +30,13 @@ func TestPart2Example(t *testing.T) {
 	b = BuildFiveX(b)
 	p := PathTo(b, util.Point{R: b.Height - 1, C: b.Width - 1})
 	p.Fill()
+	ps := PathTo(b, util.Point{R: b.Height - 1, C: b.Width - 1})
+	slowMap := ps.fillSlow()
+	fastMap := p.costMap()
+	t.Logf("slowmap\n%s", costMapStr(slowMap))
+	t.Logf("fastmap\n%s", costMapStr(fastMap))
+	require.Equal(t, slowMap, fastMap)
+	require.Equal(t, ps, p)
 	cost := p.CostFrom(util.Point{R: 0, C: 0})
 	assert.Equal(t, 315, cost)
 }
--- day15/path.go
+++ day15/path.go
@@ -9,38 +9,72 @@ import (
 
 type PathCache struct {
 	entryCosts  *util.IntBoard
-	pathCosts   *util.IntBoard
+	pathCosts   map[util.Point]int
 	destination util.Point
 }
 
 func PathTo(entryCosts *util.IntBoard, dest util.Point) *PathCache {
-	pathCosts := &util.IntBoard{
-		Values: make([]int, len(entryCosts.Values)),
-		Width:  entryCosts.Width,
-		Height: entryCosts.Height,
-	}
-	di := pathCosts.IteratorAtPoint(dest)
-	di.Set(di.SwapBoard(entryCosts).Value())
-	return &PathCache{entryCosts, pathCosts, dest}
+	return &PathCache{entryCosts, make(map[util.Point]int, len(entryCosts.Values)), dest}
 }
 
 func (p *PathCache) Valid() bool {
 	return p != nil &&
 		p.entryCosts.Valid() &&
-		p.pathCosts.Valid() &&
+		p.pathCosts != nil &&
 		p.entryCosts.IteratorAtPoint(p.destination).Valid()
 }
 
 func (p *PathCache) Fill() {
-	for i := p.pathCosts.Iterator(); i.Valid(); i = i.NextRC() {
+	// seed: the destination point always has cost 0
+	p.pathCosts[p.destination] = 0
+	// iterate improving the cost map until we stop finding any improvements. at
+	// each iteration, we only need to look at the points adjacent to those we
+	// improved on the prior iteration.
+	for improved := (map[util.Point]bool{p.destination: true}); len(improved) != 0; {
+		nextImproved := make(map[util.Point]bool)
+		for pt := range improved {
+			i := p.entryCosts.IteratorAtPoint(pt)
+			// iv is the current best cost for going from pt(i) to the dest
+			iv := p.pathCosts[pt]
+			for _, j := range i.SquareAdjacencies() {
+				if jv, ok := p.pathCosts[j.Point()]; ok {
+					// we have a known cost jv to the dest for point j adjacent to i. see
+					// if going from i to j and from there to the dest is better than the
+					// current best path for i to the dest.
+					// ev is the cost to enter point j adjacent to i
+					ev := j.Value()
+					if jv+ev < iv {
+						p.pathCosts[pt] = jv + ev
+						nextImproved[pt] = true
+					}
+				} else {
+					// we don't know a cost for going from j to the dest, so start out
+					// with going from j to i to the dest
+					jp := j.Point()
+					p.pathCosts[jp] = i.Value() + iv
+					nextImproved[jp] = true
+				}
+			}
+			improved = nextImproved
+		}
+	}
+}
+
+func (p *PathCache) fillSlow() *util.IntBoard {
+	pathCosts := &util.IntBoard{
+		Values: make([]int, len(p.entryCosts.Values)),
+		Width:  p.entryCosts.Width,
+		Height: p.entryCosts.Height,
+	}
+	for i := pathCosts.Iterator(); i.Valid(); i = i.NextRC() {
 		i.Set(math.MaxInt)
 	}
 	// seed: the destination point always has cost 0
-	p.pathCosts.IteratorAtPoint(p.destination).Set(0)
+	pathCosts.IteratorAtPoint(p.destination).Set(0)
 	// iterate improving the cost map until we stop finding any improvements
 	for improved := true; improved; {
 		improved = false
-		for i := p.pathCosts.Iterator(); i.Valid(); i = i.NextRC() {
+		for i := pathCosts.Iterator(); i.Valid(); i = i.NextRC() {
 			iv := i.Value()
 			for _, j := range i.SquareAdjacencies() {
 				jv := j.Value()
@@ -54,12 +88,30 @@ func (p *PathCache) Fill() {
 			}
 		}
 	}
+	for i := pathCosts.Iterator(); i.Valid(); i = i.NextRC() {
+		p.pathCosts[i.Point()] = i.Value()
+	}
+	return pathCosts
+}
+
+func (p *PathCache) costMap() *util.IntBoard {
+	pathCosts := &util.IntBoard{
+		Values: make([]int, len(p.entryCosts.Values)),
+		Width:  p.entryCosts.Width,
+		Height: p.entryCosts.Height,
+	}
+	for i := pathCosts.Iterator(); i.Valid(); i = i.NextRC() {
+		i.Set(math.MaxInt)
+	}
+	for pt, v := range p.pathCosts {
+		pathCosts.IteratorAtPoint(pt).Set(v)
+	}
+	return pathCosts
 }
 
 func (p *PathCache) CostFrom(start util.Point) int {
-	i := p.pathCosts.IteratorAtPoint(start)
-	if !i.Valid() {
-		panic(fmt.Errorf("invalid start point %v", start))
+	if v, ok := p.pathCosts[start]; ok {
+		return v
 	}
-	return i.Value()
+	panic(fmt.Errorf("invalid start point %v", start))
 }
